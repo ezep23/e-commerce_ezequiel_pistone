4f4927d9cd86451d63f26dee1be0506237c76439
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-09-10 20:01:30 -0300

    cards con for agregadas

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ let titulo = document.querySelector("h1");
 titulo.innerText = "PRODUCTOS";
 
 // Creando array para las cartas
-const cards = []
+const cards = [`<div></div>`];
 
 // Bucle para generar las cards 
-for (let i = 1; i <= 6; i++) {
-    const card = 
-        <div class="col-lg-4 col-md-6 mb-4"> 
-            <div class="card">
-                <img src="" class="card-img-top" alt="Producto">
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text"></p>
-                    <p class="card-text"></p>
-                    <a href="#" class="btn btn-succes">Ver más</a>
-                </div>
-            </div>
-        </div>
-    ;
+for (let i = 1; i <= 6; i++){
+    const card = `<div class="col-lg-4 col-md-6 mb-4"> 
+                        <div class="card">
+                            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Producto">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title ${i}</h5>
+                                <p class="card-text"></p>
+                                <p class="card-text"></p>
+                                <a href="#" class="btn btn-succes">Ver más</a>
+                            </div>
+                        </div> 
+                    </div>`;
     // Metiendo las cards en el array 
-    cardsArray.push(card);
-}
+    cards.push(card);
+};
+
+document.querySelector("section").innerHTML = cards.join().replaceAll(",", "");
+
